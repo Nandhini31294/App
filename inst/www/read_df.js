e471a5f12e84bfd23ed8b785ca28ddec2d491b6a
Nandhini31294 <42430670+Nandhini31294@users.noreply.github.com>
2018-08-28 11:08:12 +0530

    first commit

---
+++ inst/www/read_df.js
@@ -0,0 +1,57 @@
+//init this script when the page has loaded
+$(document).ready(function(){
+  
+  function uploadcsv(filename){
+	 alert("inside uploadcsv....");
+    //disable the button during upload
+    $("#submitbutton").attr("disabled", "disabled");        
+
+    //perform the request
+    var req = ocpu.call("read_csv", {
+      file : filename
+    }, function(session){
+      //on success call printsummary()
+      printsummary(session);
+    });
+    
+    //if R returns an error, alert the error message
+    req.fail(function(){
+      alert("Server error: " + req.responseText);
+    });
+    
+    //after request complete, re-enable the button 
+    req.always(function(){
+      $("#submitbutton").removeAttr("disabled")
+    });        
+  }    
+  
+  
+  function printsummary(mydata){
+	  alert("inside printsummary....");
+    //perform the request
+    var req = ocpu.call("printsummary", {
+      df_full : mydata
+    }, function(session){
+      session.getConsole(function(output){
+        $("#output code").text(output);
+      });
+    }).fail(function(){
+      alert("Server error: " + req.responseText);
+    });        
+  }
+  
+  $("#submitbutton").on("click", function(){
+    
+	alert("inside script....");
+    //arguments
+    //read the value for 'filename'
+	var filename = $("#uploadFile").val();
+    
+    if(!filename){
+      alert("No file selected.");
+      return;
+    }
+    
+    uploadcsv(filename);        
+  });
+});
