ec2892dd7d57f79a1e368eefd622c51cfa7fbbb9
Nandhini31294 <42430670+Nandhini31294@users.noreply.github.com>
2018-08-29 12:28:02 +0530

    Update read_csv.R

--- R/read_csv.R
+++ R/read_csv.R
@@ -9,14 +9,14 @@ read_csv<-function(filename)
 {
   library(utils)  
   if(!grepl(".csv$", filename)){
-  	stop("Uploaded filename must be a .csv filename!");
+    stop("Uploaded filename must be a .csv filename!");
   }
   #filename1<-gsub("fakepath","opencpuapp_ip",filename)
-  df_full<-utils::read.csv("c:/opencpuapp_ip/dummy.csv", header = TRUE, stringsAsFactors = FALSE );
-    
+  df_full<-utils::read.csv(filename, header = TRUE, stringsAsFactors = FALSE );
+  
   list(
-	#message = paste("hello ", paste("c:/opencpuapp_ip/",substr(filename,13,nchar(filename)),sep=""), "! This is", R.Version()$version.string)
-	message = paste("Read Successful" )
-	)
-	return(df_full)
+    #message = paste("hello ", paste("c:/opencpuapp_ip/",substr(filename,13,nchar(filename)),sep=""), "! This is", R.Version()$version.string)
+   message = paste("Read Successful" )
+  )
+  return(df_full)
 }
